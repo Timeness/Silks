1d512198b990f068ed10da83fc5041aae07d2e9a
Timeness <careless@flash.co>
2025-02-19 17:36:32 +0530

    done

---
+++ public/script.js
@@ -0,0 +1,26 @@
+let map = L.map('map').setView([20.5937, 78.9629], 5);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+let marker;
+const socket = new WebSocket("ws://localhost:3000");
+
+document.getElementById("shareLocation").addEventListener("click", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(position => {
+            const { latitude, longitude } = position.coords;
+            socket.send(JSON.stringify({ latitude, longitude }));
+        });
+    }
+});
+
+socket.onmessage = event => {
+    const { latitude, longitude } = JSON.parse(event.data);
+    const location = [latitude, longitude];
+
+    if (!marker) {
+        marker = L.marker(location).addTo(map);
+    } else {
+        marker.setLatLng(location);
+    }
+
+    map.setView(location, 13);
+};
