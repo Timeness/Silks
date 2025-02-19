cad97afede2301a59c6b1b9c0b4ca930b4b044dc
Timeness <careless@flash.co>
2025-02-19 17:46:14 +0530

    done

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 let map = L.map('map').setView([20.5937, 78.9629], 5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 let marker;
-const socket = new WebSocket("wss://your-vercel-deployment-url/ws");
+const socket = new WebSocket("wss://cryberLabs.vercel.app/ws");
 
 document.getElementById("shareLocation").addEventListener("click", () => {
     if (navigator.geolocation) {
